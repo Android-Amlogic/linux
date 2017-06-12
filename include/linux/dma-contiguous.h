cc0670db7c56382859ce71a6d1c9b37d4434a114
android <android@android.android>
2017-06-12 08:11:33 +0000

    ...

--- include/linux/dma-contiguous.h
+++ include/linux/dma-contiguous.h
@@ -111,8 +111,10 @@ static inline int dma_declare_contiguous(struct device *dev, phys_addr_t size,
 	return ret;
 }
 
+unsigned long dma_get_cma_size_int_byte(struct device *dev);
 struct page *dma_alloc_from_contiguous(struct device *dev, int count,
 				       unsigned int order);
+
 bool dma_release_from_contiguous(struct device *dev, struct page *pages,
 				 int count);
 
